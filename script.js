d1d3235d731c98d398f9a77da15f616bc0c5e82b
Souvik Ghosh <96991068+Rongho@users.noreply.github.com>
2023-05-23 11:37:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//clock hand variable
+let hourhand=document.getElementById('hourhand');
+let minhand= document.getElementById('minhand');
+let sechand= document.getElementById('sechand');
+setInterval(() => {
+    let time= new Date();
+    let hour= time.getHours()
+    let min=time.getMinutes();
+    let sec=time.getSeconds()
+    //maths
+    let hr=hour*30 + min/2;
+    let mr = min *6;
+    let sr = sec * 6;
+
+    //hand rotation
+   hourhand.style.transform =`rotate(${hr}deg)`; 
+   minhand.style.transform = `rotate(${mr}deg)`;
+   sechand.style.transform = `rotate(${sr}deg)`;
+}, 1000);
